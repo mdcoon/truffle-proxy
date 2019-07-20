cfb12509b19c87068f0fbc312104ea7441e54b73
hghotra <ghotra.harvinder@gmail.com>
2019-07-20 20:51:41 +0000

    Copy over templates into contract dir

--- create-proxy.js
+++ create-proxy.js
@@ -1,41 +1,52 @@
 const path = require('path');
 const fs = require('fs');
+const logger = require('logger').createLogger();
+
+const PLUGIN_NAME = 'truffle-proxy';
+const TEMPLATE_DIR = 'templates';
+const CONTRACT_DIR = 'contracts';
 
 const USAGE = ``;
 
-const SUMMARY = ```
+const SUMMARY = `
 Congratulations, you have just created a proxy contract!
 
 To learn more about how to integrate with a proxy contract, view Example.sol
-```;
+`;
 
 const TEMPLATES = [
-  // FIXME
+  'contract.sol'
 ];
 
 module.exports = async (config) => {
+  const commandName = config._[0];
+  const commandArguments = config._.slice(1);
 
-  if (config.help) {
+  if (commandArguments && commandArguments[0] == 'help') {
     logger.info(USAGE);
     return;
   }
 
-  const commandName = config._[0];
-  const commandArguments = config._.slice(1);
+  const templateBaseDir = path.join(config._values.working_directory, 'node_modules', PLUGIN_NAME, 'templates');
+  const contractDestination = path.join(config._values.working_directory, CONTRACT_DIR);
+  if (fs.existsSync(templateBaseDir) && fs.existsSync(contractDestination)) {
+    TEMPLATES.forEach((template) => {
+      const source = path.join(templateBaseDir, template);
+      const destination = path.join(contractDestination, template);
+      if (fs.existsSync(source)) {
+        logger.info(`Generating ${template}...`);
+        fs.copyFileSync(source, destination);
+      } else {
+        logger.error(`Missing template ${template}`)
+      }
+    });
 
-  const templateBaseDir = ''; // FIXME
-  const contractDestination = ''; // FIXME
+    // FIXME: generate migration file
+    // FIXME: what if existing migrations?
 
-  TEMPLATES.forEach((template) => {
-    const source = path.join(templateBaseDir, template);
-    const destination = path.join(contractBaseDir, template);
-    log.info(`Generating ${template}...`);
-    fs.copyFileSync(source, destination);
-  });
-
-  // FIXME: generate migration file
-  // FIXME: what if existing migrations?
-
-  logger.info(SUMMARY);
+    logger.info(SUMMARY);
+  } else {
+    // FIXME: something went wrong
+  }
 
 };
