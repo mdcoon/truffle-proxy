9b3665f28bce5930dd4a79af9bf97bd9e28fd12c
mdcoon <mdcoon1@yahoo.com>
2019-07-20 23:23:49 -0400

    Added increment functionality to sample.sol

--- templates/solidity/Sample.sol
+++ templates/solidity/Sample.sol
@@ -22,6 +22,10 @@ contract Sample  is SampleDataLayout, Owned, Proxiable, LibraryLock {
         guestBook[msg.sender] = true;
     }
 
+    function increment() public delegatedOnly {
+        count += 1;
+    }
+
     function codeAddress() public delegatedOnly returns (address cAddr) {
         assembly {
             cAddr := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
--- templates/solidity/SampleDataLayout.sol
+++ templates/solidity/SampleDataLayout.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.1;
 import './LibraryLockDataLayout.sol';
 
 contract SampleDataLayout is LibraryLockDataLayout {
-    
+    uint public count;
     uint public maxGuests;
     mapping(address=>bool) public guestBook;
 }
