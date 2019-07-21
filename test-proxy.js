a0c819f6129e4c868f95be77394c145e2ab14d69
opz <will.shahda@gmail.com>
2019-07-21 04:52:30 -0400

    Fix issue detecting deployed contracts on live chain before testing

--- test-proxy.js
+++ test-proxy.js
@@ -8,42 +8,42 @@ module.exports = (config) => {
   const test = require('truffle-core/lib/commands/test');
   const dir = require("node-dir");
 
-  const network = config['networks'][
-    config['network'] || Object.keys(config['networks'])[0]
-  ];
-
-  const protocol = 'http://';
-  const url = protocol + network.host + ':' + network.port;
+  const networkName = config['network'] || Object.keys(config['networks'])[0];
+  const network = config['networks'][networkName];
 
   const localHost = '127.0.0.1';
   const localPort = 7545;
-  const localId = 4447;
+  const protocol = 'http://';
+  const url = protocol + network.host + ':' + network.port;
 
   const web3 = new Web3(Web3.providers.HttpProvider(url));
 
   web3.eth.getBlockNumber().then(blockNumber => {
-    const options = {
-      port: localPort,
-      network_id: localId,
-      fork: url + '@' + blockNumber,
-    };
-
-    const server = ganache.server(options);
-    server.listen(localPort, (err, result) => {
-      const testOptions = {
-        _: [],
-        network: 'test-proxy',
-        networks: {
-          'test-proxy': {
-            host: localHost,
-            port: localPort,
-            network_id: localId,
-          }
-        }
+    web3.eth.getAccounts().then(accounts => {
+      const options = {
+        port: localPort,
+        network_id: network.network_id,
+        fork: url + '@' + blockNumber
       };
 
-      test.run(testOptions, err => {
-        server.close(() => {});
+      const server = ganache.server(options);
+      server.listen(localPort, (err, result) => {
+        const testOptions = {
+          _: [],
+          network: networkName,
+          networks: {
+            [networkName]: {
+              host: localHost,
+              port: localPort,
+              network_id: network.network_id,
+            }
+          }
+        };
+
+        test.run(testOptions, err => {
+          console.log(err);
+          server.close(() => {});
+        });
       });
     });
   });
