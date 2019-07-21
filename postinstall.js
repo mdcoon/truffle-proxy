2bf0c781fa91ebff43f09ad98b025e0c1f6a62d3
Mike Powers <mjpowersjr@gmail.com>
2019-07-20 20:36:47 -0400

    Added PWD detection

--- postinstall.js
+++ postinstall.js
@@ -1,4 +1,9 @@
 
 const metadata = require('./package.json');
 
+const appRoot = process.env.PWD;
+
+console.log("PWD: " + appRoot);
+
+
 console.log(`Please add ${metadata.name} to the 'plugins' section of your truffle config!`);
